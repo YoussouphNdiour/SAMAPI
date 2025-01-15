145f0903c7fd25b45375bd1999603cd2edf2cc0a
@ndiouryoussouph <youssouph.ndiour@sen-gt.com>
2025-01-15 10:42:14 +0000

    firebase

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,13 +3,14 @@ importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js');
 importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-auth.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAk7MDFEDq0sZTMU9GQ1OQ7x5TtaolSFw0",
-    authDomain: "drivevalley-fdb7f.firebaseapp.com",
-    projectId: "drivevalley-fdb7f",
-    storageBucket: "drivevalley-fdb7f.appspot.com",
-    messagingSenderId: "76471554747",
-    appId: "1:76471554747:web:63392218660695de6d0f9e",
-    measurementId: "76471554747"
+    apiKey: "AIzaSyCii-GMJatZkVscS-pJvUBx28CDjQOLXx8",
+    authDomain: "jayma-88682.firebaseapp.com",
+    databaseURL: "https://jayma-88682-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "jayma-88682",
+    storageBucket: "jayma-88682.firebasestorage.app",
+    messagingSenderId: "484779040551",
+    appId: "1:484779040551:web:773b2242f5dfdd38341302",
+    measurementId: "G-0ZX2DBWZH7"
 });
 
 const messaging = firebase.messaging();
